c47ca2b33e2ba9b1f5415dbdbf1e21f6773a4273
Ana Caroline <89099643+carolineanali@users.noreply.github.com>
2023-04-09 12:25:08 -0300

    Projeto do quiz

---
+++ src/components/perguntas.jsx
@@ -0,0 +1,17 @@
+export const perguntas = [
+    {
+        pergunta: 'Em React.Js, como são chamadas as entradas que são passadas na criação dos componentes React, usando uma convenção de nomenclatura semelhante aos atributos de tag HTML?',
+        respostas: ['Refs','Props', 'State', 'Keys', 'Elements'],   
+        correta: 'Props'  
+    },
+    {
+        pergunta: 'Qual a habilidade técnica não é necessária para ser um desenvolvedor front-end?',
+        respostas: ['HTML', 'Python', 'CSS','jQuery', 'Javascript'],
+        correta: 'Python'
+    },
+    {
+        pergunta: 'Julgue se a afirmativa a seguir é verdadeira ou falsa: O JavaScript é amplamente categorizado como uma linguagem de criação de scripts ou uma linguagem interpretada. O código em JavaScript é interpretado, ou seja, é traduzido diretamente no código de linguagem de máquina subjacente por um mecanismo de JavaScript.',
+        respostas: ['Verdadeira', 'Falsa'],
+        correta: 'Verdadeira'
+    }
+    ];
---
+++ src/components/quiz.jsx
@@ -0,0 +1,17 @@
+import {perguntas} from './perguntas';
+import React, { useState } from 'react';
+
+
+const quiz = () => {
+    const [respostaCorreta, setRespostaCorreta] = useState(0);
+    const [perguntaAtual, setPerguntaAtual] = useState(0);
+    const [respostaAtual, setRespostaAtual] = useState(null);
+
+
+
+  return (
+    <div>quiz</div>
+  )
+}
+
+export default quiz
