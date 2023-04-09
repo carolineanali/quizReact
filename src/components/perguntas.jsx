9b24dc0c9f332f0e12972e2ae4b6872c56f84578
Ana Caroline <89099643+carolineanali@users.noreply.github.com>
2023-04-09 16:30:05 -0300

    alterações nas perguntas

--- src/components/perguntas.jsx
+++ src/components/perguntas.jsx
@@ -1,17 +1,27 @@
 export const perguntas = [
     {
         pergunta: 'Em React.Js, como são chamadas as entradas que são passadas na criação dos componentes React, usando uma convenção de nomenclatura semelhante aos atributos de tag HTML?',
-        respostas: ['Refs','Props', 'State', 'Keys', 'Elements'],   
-        correta: 'Props'  
+        opcaoRespostas: [
+            {resposta: 'Refs', iscorrect: false},
+            {resposta: 'Props', iscorect: true},
+            {resposta: 'State', iscorrect: false}, 
+            {resposta: 'Keys', iscorrect: false}, 
+            {resposta: 'Elements', iscorrect: false}
+        ]  
     },
     {
         pergunta: 'Qual a habilidade técnica não é necessária para ser um desenvolvedor front-end?',
-        respostas: ['HTML', 'Python', 'CSS','jQuery', 'Javascript'],
-        correta: 'Python'
+        opcaoRespostas: [
+            {resposta: 'HTML', iscorrect: false},
+            {resposta: 'Python', iscorrect: true }, 
+            {resposta: 'CSS', iscorrect: false},
+            {resposta: 'jQuery', iscorrect: false},
+            {resposta: 'Javascript', iscorrect: false}]
     },
     {
         pergunta: 'Julgue se a afirmativa a seguir é verdadeira ou falsa: O JavaScript é amplamente categorizado como uma linguagem de criação de scripts ou uma linguagem interpretada. O código em JavaScript é interpretado, ou seja, é traduzido diretamente no código de linguagem de máquina subjacente por um mecanismo de JavaScript.',
-        respostas: ['Verdadeira', 'Falsa'],
-        correta: 'Verdadeira'
+        opcaoRespostas: [
+            {resposta: 'Verdadeira', iscorrect: true}, 
+            {resposta:'Falsa', iscorrect: false}]
     }
     ];
