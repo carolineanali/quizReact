7c72dea75d0670bbb0a66e7ef17a76b539fc03f3
Ana Caroline <89099643+carolineanali@users.noreply.github.com>
2023-04-12 19:56:05 -0300

    alterações no componente das perguntas

--- src/components/perguntas.jsx
+++ src/components/perguntas.jsx
@@ -1,27 +1,26 @@
-export const perguntas = [
-    {
-        pergunta: 'Em React.Js, como são chamadas as entradas que são passadas na criação dos componentes React, usando uma convenção de nomenclatura semelhante aos atributos de tag HTML?',
-        opcaoRespostas: [
-            {resposta: 'Refs', iscorrect: false},
-            {resposta: 'Props', iscorect: true},
-            {resposta: 'State', iscorrect: false}, 
-            {resposta: 'Keys', iscorrect: false}, 
-            {resposta: 'Elements', iscorrect: false}
-        ]  
-    },
-    {
-        pergunta: 'Qual a habilidade técnica não é necessária para ser um desenvolvedor front-end?',
-        opcaoRespostas: [
-            {resposta: 'HTML', iscorrect: false},
-            {resposta: 'Python', iscorrect: true }, 
-            {resposta: 'CSS', iscorrect: false},
-            {resposta: 'jQuery', iscorrect: false},
-            {resposta: 'Javascript', iscorrect: false}]
-    },
-    {
-        pergunta: 'Julgue se a afirmativa a seguir é verdadeira ou falsa: O JavaScript é amplamente categorizado como uma linguagem de criação de scripts ou uma linguagem interpretada. O código em JavaScript é interpretado, ou seja, é traduzido diretamente no código de linguagem de máquina subjacente por um mecanismo de JavaScript.',
-        opcaoRespostas: [
-            {resposta: 'Verdadeira', iscorrect: true}, 
-            {resposta:'Falsa', iscorrect: false}]
-    }
-    ];
+import React from 'react';
+
+    const Pergunta = ({ pergunta, opcoes, opcaoSelecionada, aoSelecionarOpcao }) => {
+      return (
+        <div>
+          <h2>{pergunta}</h2>
+          {opcoes.map((opcao) => (
+            <div key={opcao}>
+              <label>
+                <input
+                  type="radio"
+                  name={pergunta}
+                  value={opcao}
+                  checked={opcaoSelecionada === opcao}
+                  onChange={() => aoSelecionarOpcao(opcao)}
+                />
+                {opcao}
+              </label>
+            </div>
+          ))}
+        </div>
+      );
+    };
+    
+    export default Pergunta;
+    
